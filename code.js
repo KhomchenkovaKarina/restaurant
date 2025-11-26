d15c48d3b15ced275cf02d204674c51a7f89b4fd
Karina <homcenkovakarina@gmail.com@>
2025-11-26 19:28:40 +0600

    добавила вкладку меню и контакты, добавила фото для соответствующих вкладок, добавила возможность листать слайдеры с помощью библиотеки swiper

---
+++ code.js
@@ -0,0 +1,15 @@
+new Swiper('.image-slider', {
+    //arrows
+    navigation: {
+        nextEl: '.gallery__right-arrow',
+        prevEl: '.gallery__left-arrow',
+    },
+})
+
+new Swiper('.reviews', {
+    //arrows
+    navigation: {
+        nextEl: '.reviews__arrow-right',
+        prevEl: '.reviews__arrow-left',
+    },
+})
